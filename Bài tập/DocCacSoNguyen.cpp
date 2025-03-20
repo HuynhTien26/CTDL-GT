477bec0f5697547d96053c8e7343e45d4e1c0eab
Huỳnh Thị Thủy Tiên <thtien.2606@gmail.com>
2025-03-20 11:56:53 +0700

    Update DocCacSoNguyen.cpp

--- Bài tập/DocCacSoNguyen.cpp
+++ Bài tập/DocCacSoNguyen.cpp
@@ -1,18 +1,24 @@
 #include <iostream>
 #include <stdio.h>
 using namespace std;
+
 #define F1 "DaySoNguyen.inp" 
+
 int main(){
 	FILE *fptr;
 	fptr = fopen(F1, "r");
+	
 	if (fptr ==NULL){
 		cout<<"Khong the mo file";
 		return 1;
 	}
+	
 	 char myString [100];
-	 if (fgets(myString, 100, fptr) != NULL){
+	
+	 while (fgets(myString, sizeof(myString), fptr) != NULL){
 	 cout<<myString;
-	 };
+	 }
+	
 	 fclose(fptr);
 	 return 0;
 }
